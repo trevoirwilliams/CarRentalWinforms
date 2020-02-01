e8ded32f95fb3429a9eb67193346991c1ce404ac
Trevoir Williams <trevoir.williams@gmail.com>
2020-02-01 13:05:56 -0500

    Add project files.

---
+++ CarRentalGroup5/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace CarRentalGroup5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtOwner = new System.Windows.Forms.TextBox();
+            this.cbCarList = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dtRentDate = new System.Windows.Forms.DateTimePicker();
+            this.dtReturnDate = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtCustomerName = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtCost = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.MenuText;
+            this.label1.Font = new System.Drawing.Font("Matura MT Script Capitals", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Location = new System.Drawing.Point(70, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(470, 84);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Car Rental";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtOwner
+            // 
+            this.txtOwner.Location = new System.Drawing.Point(12, 168);
+            this.txtOwner.Name = "txtOwner";
+            this.txtOwner.Size = new System.Drawing.Size(243, 22);
+            this.txtOwner.TabIndex = 1;
+            // 
+            // cbCarList
+            // 
+            this.cbCarList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbCarList.FormattingEnabled = true;
+            this.cbCarList.Items.AddRange(new object[] {
+            "Honda Stream",
+            "Lada",
+            "Buggy",
+            "Rust Bucket",
+            "Probox"});
+            this.cbCarList.Location = new System.Drawing.Point(326, 164);
+            this.cbCarList.Name = "cbCarList";
+            this.cbCarList.Size = new System.Drawing.Size(242, 24);
+            this.cbCarList.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(7, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Owner";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(321, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 29);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Type Of Car";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 215);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(126, 29);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Rent Date";
+            // 
+            // dtRentDate
+            // 
+            this.dtRentDate.Location = new System.Drawing.Point(12, 247);
+            this.dtRentDate.Name = "dtRentDate";
+            this.dtRentDate.Size = new System.Drawing.Size(243, 22);
+            this.dtRentDate.TabIndex = 6;
+            // 
+            // dtReturnDate
+            // 
+            this.dtReturnDate.Location = new System.Drawing.Point(321, 247);
+            this.dtReturnDate.Name = "dtReturnDate";
+            this.dtReturnDate.Size = new System.Drawing.Size(247, 22);
+            this.dtReturnDate.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(321, 215);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(148, 29);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Return Date";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 300);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(198, 29);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Customer Name";
+            // 
+            // txtCustomerName
+            // 
+            this.txtCustomerName.Location = new System.Drawing.Point(17, 333);
+            this.txtCustomerName.Name = "txtCustomerName";
+            this.txtCustomerName.Size = new System.Drawing.Size(238, 22);
+            this.txtCustomerName.TabIndex = 9;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(321, 300);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 29);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Cost";
+            // 
+            // txtCost
+            // 
+            this.txtCost.Location = new System.Drawing.Point(326, 333);
+            this.txtCost.Name = "txtCost";
+            this.txtCost.Size = new System.Drawing.Size(242, 22);
+            this.txtCost.TabIndex = 11;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(12, 383);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(134, 29);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "ID Number";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(17, 416);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(238, 22);
+            this.textBox4.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Matura MT Script Capitals", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(321, 383);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(238, 85);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Submit ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(665, 558);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtCost);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtCustomerName);
+            this.Controls.Add(this.dtReturnDate);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dtRentDate);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbCarList);
+            this.Controls.Add(this.txtOwner);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtOwner;
+        private System.Windows.Forms.ComboBox cbCarList;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dtRentDate;
+        private System.Windows.Forms.DateTimePicker dtReturnDate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtCustomerName;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtCost;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CarRentalGroup5/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarRentalGroup5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string customerName = txtCustomerName.Text;
+                double cost = Convert.ToDouble(txtCost.Text);
+                var dateRented = dtRentDate.Value;
+                var dateReturned = dtReturnDate.Value;
+                //Use the keyword var to declare a more generic variable
+                //whose data type is determined by the value being assigned. 
+                var typeOfCar = cbCarList.Text;
+                var isValid = true;
+
+                // Adding validations to the form input
+                if (String.IsNullOrEmpty(customerName)
+                    || dateRented == null
+                    || String.IsNullOrEmpty(typeOfCar))
+                {
+                    isValid = false;
+                    MessageBox.Show("Incomplete");
+                }
+                if (dateRented > dateReturned)
+                {
+                    isValid = false;
+                    MessageBox.Show("Invalid Date Entered for Return " +
+                        "Date");
+                }
+                //if(isValid == true)
+                if (isValid)
+                {
+                    MessageBox.Show($"Customer Name: {customerName}\n\r" +
+                    $"Cost: {cost}\n\r" +
+                    $"Date Rented: {dateRented}\n\r" +
+                    $"Date Returned: {dateReturned}\n\r" +
+                    $"Type Of Car: {typeOfCar}");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                //throw;
+            }
+            
+            
+        }
+    }
+}
